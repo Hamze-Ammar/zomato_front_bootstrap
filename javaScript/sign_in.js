67388b839ec930fd503ae9a80d8b728649b7b1dc
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-08 09:11:21 +0300

    Fixed sign in page fetch

--- javaScript/sign_in.js
+++ javaScript/sign_in.js
@@ -1,16 +1,28 @@
 console.log("hello hamze");
 
-// data to be sent to the POST request
-let _data = {
-    email: $("#floatingInput").val(),
-    password: $("#floatingPassword").val(),
-  }
-  
-fetch('http://127.0.0.1:8000/api/sign_in', {
-method: "POST",
-body: JSON.stringify(_data),
-headers: {"Content-type": "application/json; charset=UTF-8"}
+$("#submit").click(function(e){
+    e.preventDefault();
+    let email = $("#floatingInput").val();
+    let pass = $("#floatingPassword").val();
+
+
+    // data to be sent to the POST request
+    let _data = {
+        email: email,
+        password: pass,
+    }
+    
+    fetch('http://127.0.0.1:8000/api/sign_in', {
+    method: "POST",
+    body: JSON.stringify(_data),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+
 })
-.then(response => response.json()) 
-.then(json => console.log(json))
-.catch(err => console.log(err));
+
+
+
+
